859b021ff5cae3daf4a5c2865857414a175ad550
Chetan <chetan.mn9@gmail.com>
2025-06-01 19:52:38 +1000

    Initial Commit

---
+++ ShowList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShowList
+//
+//  Created by Chetan on 1/6/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShowListTests/ShowListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShowListTests.swift
+//  ShowListTests
+//
+//  Created by Chetan on 1/6/2025.
+//
+
+import Testing
+@testable import ShowList
+
+struct ShowListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
